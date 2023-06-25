3639b7cf2a4dca36a2eb2421adb1cf95428b7f90
princebak <princebakenga09032017@gmail.com>
2023-06-25 14:20:17 +0100

    update: tech section updated

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,8 +6,17 @@ const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((tech) => (
-        <div className="w-28 h-28" key={tech.name}>
-          <BallCanvas icon={tech.icon} />
+        <div
+          className="w-28 h-28 rounded-full border border-white flex justify-center items-center"
+          key={tech.name}
+        >
+          <img
+            src={tech.icon}
+            alt="tech"
+            width="75px"
+            height="75px"
+            className="rounded-full"
+          />
         </div>
       ))}
     </div>
